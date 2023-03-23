3d0e1cb06393c3545ea26758a1fbec662575d7db
Jardel Bordignon <jardel1101@gmail.com>
2023-03-23 03:20:36 -0300

    refactor: react-native-rename

--- client/ios/HealthGymTests/HealthGymTests.m
+++ client/ios/HealthGymTests/HealthGymTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface clientappTests : XCTestCase
+@interface HealthGymTests : XCTestCase
 
 @end
 
-@implementation clientappTests
+@implementation HealthGymTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
